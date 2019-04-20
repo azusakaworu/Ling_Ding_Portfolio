7cec551d8f220039730f1c34024805416589c480
Ling Ding <azusakaworu@gmail.com>
2019-04-20 00:21:42 -0400

    structor

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ling_Ding_Portfolio</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+
+
+<h1>Ling_Ding_Portfolio</h1>
+<?php include 'templetes/header.html';?>
+
+
+
+<!-- self introduce -->
+<section>
+    <h2>About Me</h2>
+    <h3>Resume <a href="https://resume.io/r/1UhVxeC9S">Resume</a></h3>
+
+</section>
+
+
+
+
+
+<!-- projects -->
+<section>
+    <h2>Projects</h2>
+<ul>
+    <li><a href="projects/github/github.html">GitHub</a></li>
+    <li><a href="projects/foundation/foundation.html">foundation</a></li>
+    <li><a href="projects/bootstrap/bootstrap.html">bootstrap</a></li>
+    <li><a href="projects/SASS/SASS.html">SASS</a></li>
+    <li><a href="projects/nodejs/nodejs.html">nodejs</a></li>
+    <li><a href="projects/Vue/Vue.html">Vue</a></li>
+    <li><a href="projects/jQuery/jQuery.html">jQuery</a></li>
+</ul>
+</section>
+
+
+<!-- contact me -->
+<section>
+    <h2>Contact Me</h2>
+</section>
+
+
+
+<?php include "templetes/footer.html";?>
+
+
+<script src="js/main.js"></script>
+</body>
+</html>
